4c3f8327d65cb34e39a54dc253ead9d79144f6ed
KEH-RSAHM <282167981+KEH-RSAHM@users.noreply.github.com>
2026-09-02 06:25:59 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.SIGAP_SUPABASE_CONFIG = {
+  url: "https://fltctnkdyubgkzzjicwm.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsdGN0bmtkeXViZ2t6emppY3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3MTA0MTksImV4cCI6MjEwMzI4NjQxOX0.9AG9i11HayfmgH_fkWJiV2mbv3d3QiJKLMsO7bYj1ag"
+};
+
+window.SIGAP_SUPABASE_URL = window.SIGAP_SUPABASE_CONFIG.url;
+window.SIGAP_SUPABASE_ANON_KEY = window.SIGAP_SUPABASE_CONFIG.anonKey;
